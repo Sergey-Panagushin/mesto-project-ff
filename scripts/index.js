7e98ba0d49b2955d76c9531b5bb6f72752ad2c8f
Sergey-Panagushin <faceortable@gmail.com>
2025-02-10 12:55:58 +0300

    доделал

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const page = document.querySelector(".page");
+const pageContent = page.querySelector(".page__content");
+const mainContent = pageContent.querySelector(".content");
+const placeList = mainContent.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+// @todo: Темплейт карточки
+const template = document.querySelector("#card-template").content;
 
-// @todo: Функция удаления карточки
+// @todo: Функция добавления карточки
+function createCard(addCard, deleteCard) {
+    const cardElement = template.querySelector(".card").cloneNode(true);
+    cardElement.querySelector(".card__title").textContent = addCard.name;
+    cardElement.querySelector(".card__image").src = addCard.link;
+
+    placeList.append(cardElement);
+
+    const buttonDelete = cardElement.querySelector(".card__delete-button");
+
+    deleteCard = buttonDelete.closest(".card");
+    buttonDelete.addEventListener("click", () => delButton(deleteCard));
+}
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(createCard);
+
+// @todo: Функция удаления карточки
+function delButton(deleteCard) {
+    deleteCard.remove();
+}
